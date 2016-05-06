ae7f69965ef2ab3d64893abbf200f6e5f3e5c9a1
Paul Sachs <paul.sachs@influitive.com>
2016-05-06 17:52:07 -0400

    Added option to use flex icon for centering.

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React, { PropTypes } from 'react';
 import classnames from 'classnames';
 import styles from '../lib/styles.css';
 
-const Icon = ({icon}) =>
+const Icon = ({icon, flex}) =>
   <span className={
-    classnames(styles.icon, styles[icon])
+    classnames(styles.icon, styles[icon], {[`${styles.flex}`]: flex})
   } />;
 
 
 Icon.propTypes = {
-  icon: PropTypes.string.isRequired
+  icon: PropTypes.string.isRequired,
+  flex: PropTypes.bool
 };
 
 export { Icon };
